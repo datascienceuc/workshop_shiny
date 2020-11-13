9d287dcf335c80c7e84952d978b192edb77d9bfb
datascienceuc <72874595+datascienceuc@users.noreply.github.com>
2020-11-13 17:55:44 -0300

    Add files via upload

---
+++ dashboard.R
@@ -0,0 +1,12 @@
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody()
+)
+
+server <- function(input, output){}
+
+shinyApp (ui = ui, server = server)
